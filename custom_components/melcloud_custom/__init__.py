fbe5d0a8a633102029ea3400ab63f4074b219a21
ollo69 <oliveroandrea@libero.it>
2022-07-23 01:29:27 +0200

    Maintenance
    - Use async_forward_entry_setups from HA 2022.8
    - Store token instead of password in config-entry
    - Minor improvement in additional sensors

--- custom_components/melcloud_custom/__init__.py
+++ custom_components/melcloud_custom/__init__.py
@@ -1,19 +1,31 @@
 """The MELCloud Climate integration."""
+from __future__ import annotations
+
+from aiohttp import ClientConnectionError
 import asyncio
+from async_timeout import timeout
+from collections.abc import Iterable
 from datetime import timedelta
 import logging
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
-from aiohttp import ClientConnectionError, ClientSession
-from async_timeout import timeout
 from pymelcloud import Device, get_devices
 from pymelcloud.client import BASE_URL
 import voluptuous as vol
 
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
-from homeassistant.const import ATTR_MODEL, CONF_USERNAME, CONF_PASSWORD, Platform
+from homeassistant.const import (
+    ATTR_MODEL,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_TOKEN,
+    MAJOR_VERSION,
+    MINOR_VERSION,
+    Platform,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.typing import ConfigType
@@ -69,50 +81,104 @@ CONFIG_SCHEMA = vol.Schema(
 
 
 class MelCloudAuthentication:
-    def __init__(self, email, password, language = Language.English):
+    """Manage authentication to MelCloud retrieving a valid token."""
+
+    def __init__(self, email, password, language=Language.English):
+        """Init MelCloudAuthentication."""
         self._email = email
         self._password = password
         self._language = language
-        self._contextkey = None
+        self._context_key = None
 
-    def isLogin(self):
-        return self._contextkey != None
-        
-    async def login(self, _session: ClientSession):
+    async def login(self, hass: HomeAssistant):
+        """Try login MelCloud with provided credential."""
         _LOGGER.debug("Login ...")
 
-        self._contextkey = None
+        self._context_key = None
+        session = async_get_clientsession(hass)
 
-        if _session is None:
-            return False
-            
         body = {
             "Email": self._email,
             "Password": self._password,
             "Language": self._language,
             "AppVersion": "1.19.1.1",
-            "Persist": False,
+            "Persist": True,
             "CaptchaResponse": None,
         }
 
-        async with _session.post(
+        async with session.post(
             f"{BASE_URL}/Login/ClientLogin", json=body, raise_for_status=True
         ) as resp:
             req = await resp.json()
     
-        if not req is None:
-            if "ErrorId" in req and req["ErrorId"] == None:
-                self._contextkey = req.get("LoginData").get("ContextKey")
-                return True
-            else:
-                _LOGGER.error("MELCloud User/Password invalid!")
-        else:
-            _LOGGER.error("Login to MELCloud failed!")
-            
+        if req is not None:
+            if "ErrorId" in req:
+                if req["ErrorId"] is not None:
+                    _LOGGER.error("MELCloud User/Password invalid!")
+                    return False
+                if "LoginData" in req:
+                    if context_key := req["LoginData"].get("ContextKey"):
+                        self._context_key = context_key
+                        return True
+
+        _LOGGER.error("Login to MELCloud failed!")
         return False
-        
-    def getContextKey(self):
-        return self._contextkey
+
+    @property
+    def auth_token(self):
+        """Get the authorization token."""
+        return self._context_key
+
+
+def is_min_ha_version(min_ha_major_ver: int, min_ha_minor_ver: int) -> bool:
+    """Check if HA version at least a specific version."""
+    return (
+        MAJOR_VERSION > min_ha_major_ver or
+        (MAJOR_VERSION == min_ha_major_ver and MINOR_VERSION >= min_ha_minor_ver)
+    )
+
+
+async def async_setup_entity_platforms(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    platforms: Iterable[Platform | str],
+) -> None:
+    """Set up entity platforms using new method from HA version 2022.8."""
+    if is_min_ha_version(2022, 8):
+        await hass.config_entries.async_forward_entry_setups(config_entry, platforms)
+    else:
+        hass.config_entries.async_setup_platforms(config_entry, platforms)
+
+
+async def _async_migrate_config(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> None:
+    """Migrate config entry storing token instead of username and password"""
+    conf = entry.data
+    username = conf[CONF_USERNAME]
+    language = conf[CONF_LANGUAGE]
+    mc_language = LANGUAGES[language]
+
+    _LOGGER.info(
+        "Migrating %s platform config with user: %s - language: %s(%s).",
+        DOMAIN,
+        username,
+        language,
+        str(mc_language)
+    )
+
+    mcauth = MelCloudAuthentication(username, conf[CONF_PASSWORD], mc_language)
+    try:
+        result = await mcauth.login(hass)
+        if not result:
+            raise ConfigEntryNotReady()
+    except Exception as ex:
+        raise ConfigEntryNotReady() from ex
+
+    token = mcauth.auth_token
+    hass.config_entries.async_update_entry(entry, data={CONF_TOKEN: token})
+    return token
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
@@ -135,47 +201,31 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Establish connection with MELCloud."""
     conf = entry.data
-    username = conf[CONF_USERNAME]
-    language = conf[CONF_LANGUAGE]
-    mclanguage = LANGUAGES[language]
+    if CONF_TOKEN not in conf:
+        token = await _async_migrate_config(hass, entry)
+    else:
+        token = conf[CONF_TOKEN]
 
-    _LOGGER.info(
-        "Initializing %s platform with user: %s - language: %s(%s).",
-        DOMAIN,
-        username,
-        language,
-        str(mclanguage)
-    )
-    
-    mcauth = MelCloudAuthentication(username, conf[CONF_PASSWORD], mclanguage)
-    try:
-        result = await mcauth.login(hass.helpers.aiohttp_client.async_get_clientsession())
-        if not result:
-            raise ConfigEntryNotReady()
-    except Exception as ex:
-        raise ConfigEntryNotReady() from ex
-
-    token = mcauth.getContextKey()
     mel_devices = await mel_devices_setup(hass, token)
     hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {}).update(
         {
             MEL_DEVICES: mel_devices,
         }
     )
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await async_setup_entity_platforms(hass, entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(
+    if unload_ok := await hass.config_entries.async_unload_platforms(
         config_entry, PLATFORMS
-    )
-    if unload_ok:
+    ):
         hass.data[DOMAIN].pop(config_entry.entry_id)
-    if not hass.data[DOMAIN]:
-        hass.data.pop(DOMAIN)
+        if not hass.data[DOMAIN]:
+            hass.data.pop(DOMAIN)
+
     return unload_ok
 
 
@@ -188,10 +238,12 @@ class MelCloudDevice:
         self.name: Optional[str] = device.name
         self._available = True
         self._extra_attributes = None
+        self._dev_conf = None
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     async def async_update(self, **kwargs):
         """Pull the latest data from MELCloud."""
+        self._dev_conf = None
         try:
             await self.device.update()
             self._available = True
@@ -226,30 +278,30 @@ class MelCloudDevice:
     @property
     def device_conf(self):
         """Return device_conf of the device."""
-        return self.device._device_conf
+        if self._dev_conf is None:
+            dev_conf = self.device._device_conf
+            if dev_conf is None:
+                self._dev_conf = {}
+            else:
+                self._dev_conf = dev_conf.get("Device", {})
+        return self._dev_conf
 
     @property
     def wifi_signal(self) -> Optional[int]:
         """Return wifi signal."""
-        if self.device._device_conf is None:
-            return None
-        return self.device._device_conf.get("Device", {}).get("WifiSignalStrength", None)
+        return self.device_conf.get("WifiSignalStrength")
 
     @property
     def error_state(self) -> Optional[bool]:
         """Return error_state."""
-        if self.device._device_conf is None:
+        if not self.device_conf:
             return None
-        device = self.device._device_conf.get("Device", {})
-        return device.get("HasError", False)
+        return self.device_conf.get("HasError", False)
 
     @property
     def has_wide_van(self) -> Optional[bool]:
         """Return has wide van info."""
-        if self.device._device_conf is None:
-            return False
-        device = self.device._device_conf.get("Device", {})
-        return device.get("HasWideVane", False)
+        return self.device_conf.get("HasWideVane", False)
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -294,9 +346,9 @@ class MelCloudDevice:
         return data
 
 
-async def mel_devices_setup(hass, token) -> Dict[str, List[MelCloudDevice]]:
+async def mel_devices_setup(hass: HomeAssistant, token: str) -> dict[str, list[MelCloudDevice]]:
     """Query connected devices from MELCloud."""
-    session = hass.helpers.aiohttp_client.async_get_clientsession()
+    session = async_get_clientsession(hass)
     try:
         with timeout(10):
             all_devices = await get_devices(
@@ -308,7 +360,7 @@ async def mel_devices_setup(hass, token) -> Dict[str, List[MelCloudDevice]]:
     except (asyncio.TimeoutError, ClientConnectionError) as ex:
         raise ConfigEntryNotReady() from ex
 
-    wrapped_devices = {}
+    wrapped_devices: dict[str, list[MelCloudDevice]] = {}
     for device_type, devices in all_devices.items():
         wrapped_devices[device_type] = [MelCloudDevice(device) for device in devices]
     return wrapped_devices
--- custom_components/melcloud_custom/config_flow.py
+++ custom_components/melcloud_custom/config_flow.py
@@ -6,7 +6,7 @@ from aiohttp import ClientError, ClientResponseError
 from async_timeout import timeout
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_TOKEN, CONF_USERNAME
 from homeassistant.core import callback
 
 from .const import DOMAIN, CONF_LANGUAGE, LANGUAGES  # pylint: disable=unused-import
@@ -20,15 +20,12 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
 
-    async def _create_entry(self, user_input):
+    async def _create_entry(self, username: str, token: str):
         """Register new entry."""
-        username = user_input[CONF_USERNAME]
-        password = user_input[CONF_PASSWORD]
-
         await self.async_set_unique_id(username)
-        self._abort_if_unique_id_configured({CONF_PASSWORD: password})
+        self._abort_if_unique_id_configured({CONF_TOKEN: token})
         return self.async_create_entry(
-            title=username, data=user_input,
+            title=username, data={CONF_TOKEN: token},
         )
 
     async def _create_client(
@@ -47,8 +44,8 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         try:
             with timeout(10):
-                result = await self._test_authorization(username, password, language)
-                if not result:
+                token = await self._test_authorization(username, password, language)
+                if not token:
                     return self._show_form({"base": "invalid_auth"})
 
         except ClientResponseError as err:
@@ -58,11 +55,13 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         except (asyncio.TimeoutError, ClientError):
             return self._show_form({"base": "cannot_connect"})
 
-        return await self._create_entry(user_input)
+        return await self._create_entry(username, token)
 
     async def _test_authorization(self, username, password, language):
         mcauth = MelCloudAuthentication(username, password, LANGUAGES[language])
-        return await mcauth.login(self.hass.helpers.aiohttp_client.async_get_clientsession())
+        if await mcauth.login(self.hass):
+            return mcauth.auth_token
+        return None
 
     async def async_step_user(self, user_input=None):
         """User initiated config flow."""
@@ -73,6 +72,10 @@ class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_import(self, import_config):
         """Import a config entry."""
+        username = import_config[CONF_USERNAME]
+        for unique_id in self._async_current_ids():
+            if unique_id == username:
+                return self.async_abort(reason="already_imported")
         return await self._create_client(import_config)
 
     @callback
